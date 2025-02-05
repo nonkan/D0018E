40be511a807b8d111f7fd0c8b1a5cce91867aef0
Tomtentxt <114141867+Tomtentxt@users.noreply.github.com>
2025-02-05 15:35:25 +0100

    Skiter i gunicorn

--- app.py
+++ app.py
@@ -4,23 +4,18 @@ import psycopg2
 app = Flask(__name__)
 
 def connect_db():
-    try:
-        conn = psycopg2.connect(
-            dbname="d0018e_retailer",
-            user="d0018e",
-            password="pass",
-            host="localhost",  # Update as needed
-            port="5432"
-        )
-        return conn
-    except Exception as e:
-        print("Unable to connect to the database:", e)
-        return None
+    conn = psycopg2.connect(
+        dbname="d0018e_retailer",
+        user="d0018e",
+        password="pass",
+        host="localhost",
+        port="5432"
+    )
+    return conn
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    # For Gunicorn, app.run() isn't needed, it's handled by Gunicorn
-    app.config["ENV"] = "production"  # Set to production environment
+    app.run(debug=True)
