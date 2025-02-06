e760ca668b31f966b3d5f63511027c87bd6c481c
on-gadha <samuel.osterberg@hotmail.com>
2025-02-06 19:20:58 +0100

    lade till insert

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, redirect, url_for
+from flask import Flask, jsonify, render_template, redirect, url_for, request
 import psycopg2
 
 app = Flask(__name__)
@@ -26,8 +26,8 @@ def home():
     #return "Startsida"
     return render_template('index.html')
 
-@app.route('/button_clicked')
-def button_clicked():
+@app.route('/show_stock')
+def show_stock():
     conn = connect_db()
     cur = conn.cursor()
     stock = "SELECT * FROM test"
@@ -45,6 +45,33 @@ def button_clicked():
     #prints out on webpage stock.html
     return render_template('stock.html', results=results)
     #return ".."
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    item_ID = request.form['item_ID']
+    amount = request.form['amount']
+    color = request.form['color']
+    model = request.form['model']
+    price = request.form['price']
+    order_ID = request.form['order_ID']
+    customer = request.form['customer']
+
+    conn = connect_db()
+    cur = conn.cursor()
+    
+    insert_query = """
+        INSERT INTO test (item_ID, amount, color, model, price, order_ID, customer)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """
+    cur.execute(insert_query, (item_ID, amount, color, model, price, order_ID, customer))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # Return a success message as a JSON response
+    return jsonify({"message": "inserted"})
 
 if __name__ == '__main__':
     app.run(debug=True)
